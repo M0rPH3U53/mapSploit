82538e9a80cf6e6cb73f603f4ba787a8505fc756
M0rPH3U53 <144029241+M0rPH3U53@users.noreply.github.com>
2026-05-05 19:02:59 +0200

    Create mapSploit.sh

---
+++ mapSploit.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+cat <<"EOF"
+  
+                                      █████████            ████            ███   █████   
+                                     ███▒▒▒▒▒███          ▒▒███           ▒▒▒   ▒▒███    
+ █████████████    ██████   ████████ ▒███    ▒▒▒  ████████  ▒███   ██████  ████  ███████  
+▒▒███▒▒███▒▒███  ▒▒▒▒▒███ ▒▒███▒▒███▒▒█████████ ▒▒███▒▒███ ▒███  ███▒▒███▒▒███ ▒▒▒███▒   
+ ▒███ ▒███ ▒███   ███████  ▒███ ▒███ ▒▒▒▒▒▒▒▒███ ▒███ ▒███ ▒███ ▒███ ▒███ ▒███   ▒███    
+ ▒███ ▒███ ▒███  ███▒▒███  ▒███ ▒███ ███    ▒███ ▒███ ▒███ ▒███ ▒███ ▒███ ▒███   ▒███ ███
+ █████▒███ █████▒▒████████ ▒███████ ▒▒█████████  ▒███████  █████▒▒██████  █████  ▒▒█████ 
+▒▒▒▒▒ ▒▒▒ ▒▒▒▒▒  ▒▒▒▒▒▒▒▒  ▒███▒▒▒   ▒▒▒▒▒▒▒▒▒   ▒███▒▒▒  ▒▒▒▒▒  ▒▒▒▒▒▒  ▒▒▒▒▒    ▒▒▒▒▒  
+                           ▒███                  ▒███                                    
+                           █████                 █████                                   
+                          ▒▒▒▒▒                 ▒▒▒▒▒                                    
+
+by M0rPH3U53
+      
+EOF
+
+# Couleur ASSCI
+BLEU='\033[34m'
+ROUGE='\033[0;31m'
+VERT='\033[0;32m'
+GRIS='\033[0;90m'
+RESET='\033[0m'
+BLANC='\033[1;37m'
+JAUNE='\033[0;33m'
+
+# Recupere adresse réseau + CIDR
+IP=$(ip route show | grep -E '^[0-9]' | head -1 | awk '{print $1}')
+
+# Découverte réseau d'appareil SNMP
+echo -ne "🔍 ${BLANC}Scan SNMP${RESET}..."
+hotes=$(nmap -sU -p 161 --open ${IP} -oG - | grep "161/" | awk '{print $2}')
+echo -e "${JAUNE}100%${RESET}"
+
+# Verifie si la variable est vide
+if [ -z "${hotes}" ]; then
+    echo "❌ Aucun appareil SNMP"
+    exit 1
+fi
+
+# Recupere les info de l'appareil
+echo " "
+for hote in ${hotes}; do
+   	echo "📡 ${hote}"
+   	msfconsole -q -x "use auxiliary/scanner/snmp/snmp_enum; set RHOSTS ${hote}; set verbose true; run; exit" > "${hote}-snmp.txt"
+done
